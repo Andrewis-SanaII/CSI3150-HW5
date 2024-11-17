84b2e49c7b7d6d476f4585bf0666d5f6e64f3fc9
Andrewval Yacoub <andrewvalyacoub@oakland.edu>
2024-11-17 17:13:24 -0500

    first commit

---
+++ js/sort.js
@@ -0,0 +1,48 @@
+const filterForm = document.getElementById("filterForm");
+const minPriceInput = document.getElementById("minPrice");
+const maxPriceInput = document.getElementById("maxPrice");
+const modelYearInput = document.getElementById("yearFilter");
+const manufacturerInput = document.getElementById("manufacturerFilter");
+const colorInput = document.getElementById("colorFilter");
+
+document.getElementById("filterBtn").addEventListener("click", filter);
+document.getElementById("resetBtn").addEventListener("click", reset);
+
+function reset() {
+  minPriceInput.value = "0";
+  maxPriceInput.value = "1000000";
+  modelYearInput.value = "allYears";
+  manufacturerInput.value = "allManufacturers";
+  colorInput.value = "allColors";
+  filter();
+}
+
+function filter() {
+  const minPrice = parseFloat(minPriceInput.value);
+  const maxPrice = parseFloat(maxPriceInput.value);
+  const modelYear = modelYearInput.value;
+  const manufacturer = manufacturerInput.value;
+  const color = colorInput.value;
+
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    const cardPrice = parseFloat(card.getAttribute("carPrice"));
+    const cardManufacturer = card.getAttribute("carManufacturer");
+    const cardColor = card.getAttribute("carColor");
+    const cardYear = card.getAttribute("carYear");
+
+    if (
+      cardPrice >= minPrice &&
+      cardPrice <= maxPrice &&
+      (manufacturer === "allManufacturers" ||
+        cardManufacturer === manufacturer) &&
+      (color === "allColors" || cardColor === color) &&
+      (modelYear === "allYears" || cardYear === modelYear)
+    ) {
+      card.style.display = "flex";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
